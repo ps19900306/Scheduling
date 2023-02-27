1e0d73440de74c94fd0ec0694c55f13088ffd99e
ningwenqiang <1018419783@qq.com>
2023-02-27 14:37:51 +0800

    增加多点颜色判断以及 点范围找颜色判断

--- app/src/main/java/com/nwq/function/scheduling/core_code/PixelsInfo.kt
+++ app/src/main/java/com/nwq/function/scheduling/core_code/PixelsInfo.kt
@@ -6,5 +6,21 @@ create time: 2023/2/27 11:16
 Function description:
  */
 
-data class PixelsInfo(val startX:Int,val startY: Int,val width: Int,val height: Int,val stride: Int,val offset:Int=0) {
+data class PixelsInfo(
+    val startX: Int,
+    val startY: Int,
+    val width: Int,
+    val height: Int,
+    val stride: Int,
+    val offset: Int = 0
+) {
+    companion object {
+        fun buildPixelsInfo(coordinate: Coordinate, range: Int = 0): PixelsInfo {
+            val startX = coordinate.x.toInt() - range
+            val startY = coordinate.y.toInt() - range
+            val width = range * 2 + 1
+            val height = range * 2 + 1
+            return PixelsInfo(startX, startY, width, height, width, 0)
+        }
+    }
 }
--- app/src/main/java/com/nwq/function/scheduling/core_code/img/ImgUtils.kt
+++ app/src/main/java/com/nwq/function/scheduling/core_code/img/ImgUtils.kt
@@ -10,67 +10,178 @@ import com.nwq.function.scheduling.core_code.PixelsInfo
 
 @RequiresApi(Build.VERSION_CODES.Q)
 object ImgUtils {
+
+    //多点多规则颜色判断
+    suspend fun performPointsColorVerification(
+        data: List<PointColorVerification>,
+        bitmap: Bitmap,
+        toleranceErrorNumber: Int = 0,//能够容忍几个点颜色不一致
+    ): Boolean {
+        var nowErrorCount = 0
+        data.forEach {
+            if (!performPointColorVerification(it, bitmap)) {
+                nowErrorCount++
+            }
+            if (nowErrorCount > toleranceErrorNumber)
+                return false
+        }
+        return true
+    }
+
+    //单点单规则判断
+    suspend fun performPointColorVerification(
+        data: PointColorVerification,
+        bitmap: Bitmap
+    ): Boolean {
+        return if (data is PointColorVerification.CoordinateColor) {
+            if (data.range > 0) {
+                findColorLike(
+                    PixelsInfo.buildPixelsInfo(data.coordinate, data.range),
+                    bitmap,
+                    data.colorRule,
+                    data.tolerance
+                )
+            } else {
+                judgeColorLike(data.coordinate, bitmap, data.colorRule, data.tolerance)
+            }
+        } else if (data is PointColorVerification.CoordinateRule) {
+            if (data.range > 0) {
+                findColorRule(
+                    PixelsInfo.buildPixelsInfo(data.coordinate, data.range),
+                    bitmap,
+                    data.rule
+                )
+            } else {
+                judgeColorRule(data.coordinate, bitmap, data.rule)
+            }
+        } else {
+            false
+        }
+    }
+
     /**
      * 单点颜色判断 相似度
      */
-    suspend fun judgeColorLike(coordinate: Coordinate, bitmap: Bitmap,colorRule:String,tolerance:Int=0):Boolean{
+    suspend fun judgeColorLike(
+        coordinate: Coordinate,
+        bitmap: Bitmap,
+        colorRule: String,
+        tolerance: Int = 0
+    ): Boolean {
         val color = bitmap.getColor(coordinate.x.toInt(), coordinate.y.toInt())
-        val baseColor=Color.parseColor(colorRule)
-        return  judgeColorSqrt(Color.red(baseColor),Color.blue(baseColor),Color.green(baseColor),color)<=tolerance
+        val baseColor = Color.parseColor(colorRule)
+        return judgeColorSqrt(
+            Color.red(baseColor),
+            Color.blue(baseColor),
+            Color.green(baseColor),
+            color
+        ) <= tolerance
     }
 
     /**
      * 单点颜色判断 规则约束
      */
-    suspend fun judgeColorRule(coordinate: Coordinate, bitmap: Bitmap,colorRule:ColorIdentificationRule):Boolean{
+    suspend fun judgeColorRule(
+        coordinate: Coordinate,
+        bitmap: Bitmap,
+        colorRule: ColorIdentificationRule
+    ): Boolean {
         val color = bitmap.getColor(coordinate.x.toInt(), coordinate.y.toInt())
-        return colorRule.verificationRule(color.red().toInt(),color.blue().toInt(),color.green().toInt())
+        return colorRule.verificationRule(
+            color.red().toInt(),
+            color.blue().toInt(),
+            color.green().toInt()
+        )
     }
 
 
     /**
      * 区块找颜色 找到颜色则返回true
      */
-    suspend fun findColorRule(pixelsInfo: PixelsInfo, bitmap: Bitmap, colorRule:ColorIdentificationRule):Boolean{
-        val pixels =IntArray(pixelsInfo.width*pixelsInfo.height)
-        bitmap.getPixels(pixels,pixelsInfo.offset,pixelsInfo.stride,pixelsInfo.startX,pixelsInfo.startY,pixelsInfo.width,pixelsInfo.height)
-       return  pixels.find {
-            colorRule.verificationRule(Color.red(it),Color.red(it),Color.red(it))
-        }!=null
+    suspend fun findColorRule(
+        pixelsInfo: PixelsInfo,
+        bitmap: Bitmap,
+        colorRule: ColorIdentificationRule
+    ): Boolean {
+        val pixels = IntArray(pixelsInfo.width * pixelsInfo.height)
+        bitmap.getPixels(
+            pixels,
+            pixelsInfo.offset,
+            pixelsInfo.stride,
+            pixelsInfo.startX,
+            pixelsInfo.startY,
+            pixelsInfo.width,
+            pixelsInfo.height
+        )
+        return pixels.find {
+            colorRule.verificationRule(Color.red(it), Color.red(it), Color.red(it))
+        } != null
     }
 
     /**
      * 区块找颜色 找到颜色则返回true
      */
-    suspend fun findColorLike(pixelsInfo: PixelsInfo, bitmap: Bitmap,colorRule:String,tolerance:Int=0):Boolean{
-        val pixels =IntArray(pixelsInfo.width*pixelsInfo.height)
-        val baseColor=Color.parseColor(colorRule)
-        val baseRed= Color.red(baseColor)
-        val baseBlue= Color.blue(baseColor)
+    suspend fun findColorLike(
+        pixelsInfo: PixelsInfo,
+        bitmap: Bitmap,
+        colorRule: String,
+        tolerance: Int = 0
+    ): Boolean {
+        val pixels = IntArray(pixelsInfo.width * pixelsInfo.height)
+        val baseColor = Color.parseColor(colorRule)
+        val baseRed = Color.red(baseColor)
+        val baseBlue = Color.blue(baseColor)
         val baseGreen = Color.green(baseColor)
-        bitmap.getPixels(pixels,pixelsInfo.offset,pixelsInfo.stride,pixelsInfo.startX,pixelsInfo.startY,pixelsInfo.width,pixelsInfo.height)
-        return  pixels.find {
-            judgeColorSqrt(Color.red(it),Color.blue(it),Color.green(it),baseRed,baseBlue,baseGreen)<=tolerance
-        }!=null
+        bitmap.getPixels(
+            pixels,
+            pixelsInfo.offset,
+            pixelsInfo.stride,
+            pixelsInfo.startX,
+            pixelsInfo.startY,
+            pixelsInfo.width,
+            pixelsInfo.height
+        )
+        return pixels.find {
+            judgeColorSqrt(
+                Color.red(it),
+                Color.blue(it),
+                Color.green(it),
+                baseRed,
+                baseBlue,
+                baseGreen
+            ) <= tolerance
+        } != null
     }
 
 
     /******  下面的都是私有方法 ******************/
     //判断颜色的相似度,越相似值越小 最大是100
-    private suspend fun judgeColorSqrt(imgRed: Int, imgBlue: Int, imgGreen: Int,imgColor: Color): Int{
+    private suspend fun judgeColorSqrt(
+        imgRed: Int,
+        imgBlue: Int,
+        imgGreen: Int,
+        imgColor: Color
+    ): Int {
         val red = (imgColor.red() - imgRed) / 255
-        val blue = (imgColor.blue() -imgBlue) / 255
+        val blue = (imgColor.blue() - imgBlue) / 255
         val green = (imgColor.green() - imgGreen) / 255
         val diff = kotlin.math.sqrt((red * red + blue * blue + green * green).toDouble())
-        return (diff*100).toInt()
+        return (diff * 100).toInt()
     }
 
-    private suspend fun judgeColorSqrt(imgRed: Int, imgBlue: Int, imgGreen: Int,baseRed: Int, baseBlue: Int, baseGreen: Int,): Int{
+    private suspend fun judgeColorSqrt(
+        imgRed: Int,
+        imgBlue: Int,
+        imgGreen: Int,
+        baseRed: Int,
+        baseBlue: Int,
+        baseGreen: Int,
+    ): Int {
         val red = (baseRed - imgRed) / 255
-        val blue = (baseBlue -imgBlue) / 255
+        val blue = (baseBlue - imgBlue) / 255
         val green = (baseGreen - imgGreen) / 255
         val diff = kotlin.math.sqrt((red * red + blue * blue + green * green).toDouble())
-        return (diff*100).toInt()
+        return (diff * 100).toInt()
     }
 
     //判断颜色的相似度,越相似值越小 最大是100
