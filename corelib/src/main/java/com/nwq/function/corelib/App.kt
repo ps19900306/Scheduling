eb373cfe63bf379e1e58f55d0b70d2ee28e2bb53
ningwenqiang <1018419783@qq.com>
2023-06-07 15:02:23 +0800

    调试完成分组，后面考虑是否需要分这么细致

---
+++ corelib/src/main/java/com/nwq/function/corelib/App.kt
@@ -0,0 +1,29 @@
+package com.nwq.function.corelib
+
+import android.app.Application
+import com.nwq.function.corelib.utils.ContextUtil
+import com.nwq.function.corelib.utils.sp.SP
+import timber.log.Timber
+
+/**
+create by: 86136
+create time: 2023/3/3 11:20
+Function description:
+ */
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        SP.init(this, "nwq_schedule")
+        ContextUtil.context = this
+        Timber.plant(Timber.DebugTree())
+//
+//        if (!FileUtils.orcExists()) {
+//            Timber.d("拷贝文件 onCreate App NWQ_ 2023/3/29");
+//            FileUtils.copyOrcFile(assets.open(FileUtils.languageAllName))
+//        } else {
+//            Timber.d("文件已经存在 onCreate App NWQ_ 2023/3/29");
+//        }
+    }
+}
