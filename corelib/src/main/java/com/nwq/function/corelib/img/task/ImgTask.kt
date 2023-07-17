1237c2c879c7ccddbadfaa09e61d987a55ee5b8c
ningwenqiang <1018419783@qq.com>
2023-07-17 16:58:58 +0800

    补充一个参数

--- corelib/src/main/java/com/nwq/function/corelib/img/task/ImgTask.kt
+++ corelib/src/main/java/com/nwq/function/corelib/img/task/ImgTask.kt
@@ -49,6 +49,7 @@ abstract class ImgTask(
         return true
     }
 
+    //这里有补充值
     fun getOffsetX(): Int {
         return correctModel?.getOffsetXSupple()?:0
     }
@@ -57,5 +58,14 @@ abstract class ImgTask(
         return correctModel?.getOffsetYSupple()?:0
     }
 
+    //这里没有补充值
+    fun getOffX(): Int {
+        return correctModel?.offsetX?:0
+    }
+
+    //这里没有补充值点
+    fun getOffY(): Int {
+        return correctModel?.offsetY?:0
+    }
 
 }
