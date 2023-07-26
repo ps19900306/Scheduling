2a8fcc9d15c2a6ccff2119e53d818baba1994737
ningwenqiang <1018419783@qq.com>
2023-07-26 10:07:58 +0800

    敌军数量监控判断修改

--- corelib/src/main/java/com/nwq/function/corelib/excuter/star_wars/function/EnemyMonitor.kt
+++ corelib/src/main/java/com/nwq/function/corelib/excuter/star_wars/function/EnemyMonitor.kt
@@ -1,6 +1,7 @@
 package com.nwq.function.corelib.excuter.star_wars.function
 
 import android.graphics.Bitmap
+import com.nwq.function.corelib.Constant
 import com.nwq.function.corelib.img.task.BasicImgTask
 import com.nwq.function.corelib.img.task.ImgTaskImpl1
 
@@ -22,53 +23,108 @@ class EnemyMonitor(val zeroList: Array<BasicImgTask>, val oneList: Array<BasicIm
         private const val NO_NUMBER = 0
         private const val ONE_NUMBER = 1
         private const val MULTI_NUMBER = 2
-    }
 
+
+        private const val ABSOLUTELY_SAFE = -1 //安全期
+        private const val SLIGHTLY_SAFE = 8 //有白经过
+        private const val SMALL_ENEMY = 9 //一个敌军
+        private const val VERY_DANGEROUS = 10 //非常危险
+    }
 
 
     private val numberRecord = arrayOf(0, 0, 0)
     private val safeTimeRecord = arrayOf(0L, 0L, 0L)
     private val notSafeTimeRecord = arrayOf(0L, 0L, 0L)
+    private val continueTimeRecord = arrayOf(0L, 0L, 0L)//为上次持续时间
 
-    private val continueTimeRecord = arrayOf(0L, 0L, 0L)//为偶像欧
 
-    private val size = 3
-    private var lastStatus = SAFE_TYPE
-    private var needBack = true
-
+    private var safeLevel = ABSOLUTELY_SAFE
+    private var waitTime = 0L
+    private var toBeJudged = false
 
     suspend fun updateInfo(bitmap: Bitmap) {
         zeroList.forEachIndexed { index, task ->
-            if (!task.verificationRule(bitmap)) {
+            if (task.verificationRule(bitmap)) {
+                updateNumberInfo(index, NO_NUMBER)
+            } else {
                 if (oneList[index].verificationRule(bitmap)) {
                     updateNumberInfo(index, ONE_NUMBER)
                 } else {
                     updateNumberInfo(index, MULTI_NUMBER)
                 }
-            } else {
-                updateNumberInfo(index, NO_NUMBER)
             }
         }
     }
+
+    fun isSafe(): Boolean {
+
+        if (!toBeJudged && safeLevel == ABSOLUTELY_SAFE) {
+            return true
+        }
+        toBeJudged = false
+
+        var total = 0
+        numberRecord.forEach {
+            total += it
+        }
+        if (total > 2) {
+            updateSafeLevel(VERY_DANGEROUS)
+            return false
+        } else if (total == 1) { //只有一个敌军先不判断
+            updateSafeLevel(SLIGHTLY_SAFE)
+            return false
+        } else {
+            continueTimeRecord.forEach {
+                if (it > Constant.MINUTE) {
+                    updateSafeLevel(SMALL_ENEMY)
+                }
+            }
+            continueTimeRecord[0] = 0
+            continueTimeRecord[1] = 0
+            continueTimeRecord[2] = 0
+
+
+            val nowTime = System.currentTimeMillis()
+            if (safeTimeRecord.find { nowTime - it < waitTime } == null) {
+                safeLevel = ABSOLUTELY_SAFE
+                waitTime = 0L
+                return true
+            }
+        }
+
+        return false
+    }
+
+
+    private fun updateSafeLevel(now: Int) {
+        if (now > safeLevel) {
+            safeLevel = now
+            waitTime = when (now) {
+                SLIGHTLY_SAFE -> ((Math.random() + 1) * Constant.MINUTE).toLong()
+                SMALL_ENEMY -> ((Math.random() * 3 + 5) * Constant.MINUTE).toLong()
+                VERY_DANGEROUS -> ((Math.random() * 5 + 10) * Constant.MINUTE).toLong()
+                else -> 0L
+            }
+        }
+    }
+
 
     private fun updateNumberInfo(index: Int, number: Int) {
         val lastNumber = numberRecord[index]
         if (number != lastNumber) {
-             if(number==NO_NUMBER){
-
-             }
-        }
-    }
-
-
-    suspend fun checkZero(bitmap: Bitmap): Int {
-        zeroList.forEachIndexed { index, task ->
-            if (!task.verificationRule(bitmap)) {
-                return size - index
+            toBeJudged = true
+            val nowTime = System.currentTimeMillis()
+            if (number == NO_NUMBER) {//表示刚进入安全时期
+                safeTimeRecord[index] = nowTime
+                continueTimeRecord[index] = nowTime - notSafeTimeRecord[index]
+                numberRecord[index] = number
+            } else if (lastNumber == NO_NUMBER) {//表示刚进入敌军来的时候
+                notSafeTimeRecord[index] = nowTime
+                numberRecord[index] = number
+            } else {
+                numberRecord[index] = numberRecord[index].coerceAtLeast(number)
             }
         }
-        return SAFE_TYPE
     }
-
 
 }
