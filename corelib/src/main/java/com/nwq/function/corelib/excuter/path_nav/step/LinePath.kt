a064e8571d22af2a5a489ad19ab55fd5474d26a5
ningwenqiang <1018419783@qq.com>
2023-08-17 15:48:19 +0800

    点击间隔进行处理

---
+++ corelib/src/main/java/com/nwq/function/corelib/excuter/path_nav/step/LinePath.kt
@@ -0,0 +1,13 @@
+package com.nwq.function.corelib.excuter.path_nav.step
+
+import com.nwq.function.corelib.excuter.path_nav.data.PathPoint
+
+/**
+create by: 86136
+create time: 2023/8/17 11:12
+Function description:
+ */
+
+class LinePath(list:List<PathPoint>) {
+
+}
